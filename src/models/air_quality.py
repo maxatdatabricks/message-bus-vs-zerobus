4c655e59396ffcad79751e51dbbec8388f2759dc
Max Howarth <max.howarth@databricks.com>
2025-10-09 15:43:09 -0700

    Cleanup unsued files.

--- src/models/air_quality.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from datetime import datetime
-
-class AirQuality(BaseModel):
-    device_name: str
-    temp: int
-    humidity: int
-    source: str
-    timestamp: datetime
